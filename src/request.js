65c0e052c4757dbd6f7be2f8713ddab93309a932
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2019-01-10 10:54:47 +0900

    Add request util testing

--- src/request.js
+++ src/request.js
@@ -4,12 +4,15 @@ export default class Request {
   constructor (params, Message, models) {
     const req = new Message()
     if ( params ) {
-      Object.keys(params).forEach((value, key)=>{
+      Object.keys(params).forEach((key)=>{
+        const value = params[key]
         const list = value.forEach((item)=>{
           const r = new Message[models[key]]()
-          Object.keys(item).forEach((value, key)=>{
+          Object.keys(item).forEach((key)=>{
+            const value = item[key]
+            console.log({value,key})
             if ( value !== undefined ) {
-              const setter = `set${Case.pascal(key)}`
+              const setter = `set${Case.pascal(key)}${Array.isArray(value) ? 'List' : ''}`
               if (!r[setter]) throw new Error(`Invalid request parameters. '${key}'`)
               r[setter](value)
             }
--- test/request.test.js
+++ test/request.test.js
@@ -1,13 +1,21 @@
 import _ from 'lodash'
 
 import Request from '../src/request'
+import { HelloRequest } from './grpc/helloworld_pb'
 
 describe('constructor', ()=>{
   it('returns an instance', () => {
-    const params = {}
-    const Message = ()=>{}
-    const models = {}
-    expect(new Request(params, Message, models)).toBeInstanceOf(Request)
+    const params = {
+      users: [
+        {
+          name: 'puni',
+          age: 33,
+          children: ['aaa'],
+        }
+      ]
+    }
+    const models = {users: 'User'}
+    expect(new Request(params, HelloRequest, models)).toBeInstanceOf(Request)
   })
   it('throws an error if it receives an invalid host', () => {
     expect(()=>new Request()).toThrow(Error)
