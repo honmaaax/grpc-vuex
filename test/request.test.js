626de359302d4752c7a47b03b2778bd12eaa5e64
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2019-01-10 11:32:59 +0900

    Add request util testing

--- test/request.test.js
+++ test/request.test.js
@@ -4,13 +4,30 @@ import { createRequest } from '../src/request'
 import { HelloRequest } from './grpc/helloworld_pb'
 
 describe('createRequest', ()=>{
-  it('sets parames to request object', () => {
+  it('sets parames', () => {
+    const params = {
+      users: []
+    }
+    const models = {users: 'User'}
+    const req = createRequest(params, HelloRequest, models)
+    expect(req.toObject()).toEqual({
+      usersList: []
+    })
+  })
+  it('throws an error if it receives an invalid host', () => {
+    expect(()=>createRequest()).toThrow(Error)
+  })
+  it('sets collection parames', () => {
     const params = {
       users: [
         {
           name: 'hoge',
           age: 33,
           children: ['fuga'],
+        },{
+          name: 'piyo',
+          age: 55,
+          children: ['puyo'],
         }
       ]
     }
@@ -22,11 +39,12 @@ describe('createRequest', ()=>{
           name: 'hoge',
           age: 33,
           childrenList: ['fuga']
+        },{
+          name: 'piyo',
+          age: 55,
+          childrenList: ['puyo']
         }
       ]
     })
-  })
-  it('throws an error if it receives an invalid host', () => {
-    expect(()=>createRequest()).toThrow(Error)
   })
 })
