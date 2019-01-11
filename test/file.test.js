d3e665b7908ced6358bc87352fe721a28a16ea18
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2019-01-11 10:49:06 +0900

    Refactoring

--- test/file.test.js
+++ test/file.test.js
@@ -6,19 +6,19 @@ import { readFile, writeFile } from '../src/file'
 
 describe('readFile', ()=>{
   it('returns Promise', () => {
-    expect(_.get(readFile('./test/helloworld.proto'), 'then')).toBeTruthy()
-    expect(_.isFunction(readFile('./test/helloworld.proto').then)).toBeTruthy()
+    expect(_.get(readFile('./test/grpc/helloworld.proto'), 'then')).toBeTruthy()
+    expect(_.isFunction(readFile('./test/grpc/helloworld.proto').then)).toBeTruthy()
   })
 })
 
 describe('writeFile', ()=>{
   it('returns Promise', () => {
-    const PATH = './test/dist.txt'
+    const PATH = './test/dist/dist.txt'
     expect(_.get(writeFile(PATH, 'foo'), 'then')).toBeTruthy()
     expect(_.isFunction(writeFile(PATH, 'foo').then)).toBeTruthy()
   })
   it('updates a file', () => {
-    const PATH = './test/dist2.txt'
+    const PATH = './test/dist/dist2.txt'
     return Promise.promisify(fs.writeFile)(PATH, 'foo')
       .then(()=>Promise.promisify(fs.readFile)(PATH, 'utf-8'))
       .then((res)=>{
