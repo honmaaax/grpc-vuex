d4d7d25cfce5e389e47bd99dac737ea0e9ba9637
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2019-01-15 18:29:36 +0900

    Update request util

--- src/request.js
+++ src/request.js
@@ -6,19 +6,22 @@ import Type from './type'
 // usersの中身はnew User().setName()
 // pagenationはnew GetUsersRequest().setPagination()
 // pagenationの中身はnew Pagenation().setPage()
+export function _createObjectRequest (key, value, messages) {
+  const req = new messages[key]()
+  Object.keys(value)
+    .map((key)=>[key, value[key]])
+    .forEach(([key, value])=>{
+      _createRequest(key, value, req, messages)
+    })
+  return req
+}
 export function _createRequest (key, value, request, messages) {
   if ( Type.isObject(value) ) {
-    Object.keys(value)
-      .map((key)=>[key, value[key]])
-      .forEach(([key, value])=>{
-        _createRequest(key, value, request, messages)
-      })
-    return request
+    value = _createObjectRequest(key, value, messages)
   } else if ( Type.isArray(value) ) {
-    const req = messages[key] ? new messages[key] : null
     value = value.map((value)=>{
       if ( Type.isObject(value) ) {
-        return _createRequest(key, value, req, messages)
+        return _createObjectRequest(key, value, messages)
       }
       return value
     })
